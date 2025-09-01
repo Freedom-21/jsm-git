9b690ac06e6913ab42cf1adb43534864b534fd16
Freedom-21 <126236362+Freedom-21@users.noreply.github.com>
2025-09-01 23:08:44 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,3 +17,18 @@ variable "user_alias" {
   type        = string
   description = "Username before the @ (must be unique)"
 }
+variable "client_id" {
+  type        = string
+  description = "Azure App registration (client) ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure App registration client secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Tenant ID of your Entra directory"
+}
+
